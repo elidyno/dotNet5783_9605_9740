b67617e6cc24872b545d51aee4952879fed18d3a
Avraham Meiri <ameiri181@gmail.com>
2022-11-20 00:26:19 +0200

    update DalFacade

---
+++ DalFacade/DalApi/ICrud.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    internal interface ICrud <T>
+    {
+        public T Add(T item);
+        public void Delete(T item);
+
+        public void Update (T item);
+
+        public T Get (T item);
+
+        public IEnumerable<T> GetList (T item);  
+
+
+    }
+}
