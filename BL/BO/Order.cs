be98ce95c88ebd25bb832df846700907d0900c50
Avraham Meiri <ameiri181@gmail.com>
2022-11-21 14:02:09 +0200

    Logical layer

---
+++ BL/BO/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerAdress { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime ShipDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        //public DateTime PaymentDate { get; set; }
+        public double TotalPrice { get; set; }
+        public OrderItem Items { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ BL/BO/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class OrderItem
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public int Amount { get; set; }
+        public int ProductId { get; set; }
+        public double Price { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
