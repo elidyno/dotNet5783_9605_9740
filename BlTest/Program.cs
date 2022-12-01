06d13b761b4d542744728c431ea1102a90845f70
Eli Dynovisz <eli@bizmax.co.il>
2022-12-01 20:40:43 +0200

    program Bl test fixes

--- BlTest/Program.cs
+++ BlTest/Program.cs
@@ -3,6 +3,7 @@ using Dal;
 using DalApi;
 using BlImplementation;
 using BO;
+using System.Numerics;
 
 
 namespace BlTest
@@ -12,6 +13,16 @@ namespace BlTest
         // enum for main menu
         enum MainMenuCode { Exit, Product, Order, Cart }
         // enum for sub menue: order operation
+        enum SubMenu_Product
+        {
+            ExitSubMenu = 0,
+            AddProduct,
+            DelProduct,
+            UpdateProduct,
+            ViewProduct,
+            ViewProductToCart,
+            ViewList,
+        }
         enum SubMenu_Order
         {
             ExitSubMenu = 0,
@@ -22,6 +33,7 @@ namespace BlTest
             UpdateOrderSheep
         }
         private static SubMenu_Order subMenu_Order;
+        private static SubMenu_Product subMenu_Product;
         private static MainMenuCode menuCode;
         private static IBl bl = new BlImplementation.Bl();
 
@@ -54,12 +66,12 @@ namespace BlTest
                             exit = true;
                             break;
                         default:
-                            Console.WriteLine("Invalid value, please try again\n");
+                            Console.WriteLine("Invalid number:\n Please enter one of the numbers shown in the menu\n");
                             break;
                     }
                 }
                 else
-                    Console.WriteLine("Invalid value, please try again\n");
+                    Console.WriteLine("Please entry only a intiger Number\n");
                 Console.WriteLine("press any key to continue...");
                 Console.ReadKey();
                 Console.Clear();
@@ -188,6 +200,156 @@ namespace BlTest
 
                 }
 
+            } while (!exit);
+        
+        }
+        
+        /// <summary>
+        /// chack operation of Product in BlImplementation
+        /// </summary>
+        static void ProductMenu()
+        {
+            bool success = false;
+            bool exit = false;
+            int id;
+            string name;
+            double price;
+            BO.Category category;
+            int amount;
+            BO.Product product = new();
+            do
+            {
+                Console.WriteLine(
+                      "Select operation to test:\n" +
+                      "  1) To add a Product\n" +
+                      "  2) To del a Product\n" +
+                      "  3) To update a Product\n" +
+                      "  4) To show a product - Admin screan\n" +
+                      "  5) To show a product - Cart screan\n" +
+                      "  6) To show list of all product\n" +
+                      "  0) To exit from sub menu\n"
+                       );
+                try
+                {
+                    success = SubMenu_Product.TryParse(Console.ReadLine(), out subMenu_Product);
+                    if (success)
+                    {
+                        switch (subMenu_Product)
+                        {
+                            case SubMenu_Product.ExitSubMenu:
+                                exit = true;
+                                break;
+                            case SubMenu_Product.AddProduct:
+                                //requst data of new product to add
+                                Console.WriteLine(" Enter Id number of new Product");
+                                success = int.TryParse(Console.ReadLine(), out id);
+                                if (!success)
+                                    throw new InvalidInputFormatException("Please entry only a intiger Number\n");
+                                name = Console.ReadLine();
+                                success = double.TryParse(Console.ReadLine(), out price);
+                                if (!success)
+                                    throw new InvalidInputFormatException("Please entry only a double Number\n");
+                                success = Category.TryParse(Console.ReadLine(), out category);
+                                if (!success)
+                                    throw new InvalidInputFormatException("Please entry only a intiger Number\n");
+                                success = int.TryParse(Console.ReadLine(), out amount);
+                                if (!success)
+                                    throw new InvalidInputFormatException("Please entry only a intiger Number\n");
+
+                                //creat a new product and try to add it to database
+                                product.Id = id;
+                                product.Name = name;
+                                product.Price = price;
+                                product.Category = category;
+                                product.InStock = amount;
+                                bl.Product.Add(product);
+                                break;
+                            case SubMenu_Order.ViewAll:
+                                IEnumerable<BO.OrderForList> ordersForList = new List<BO.OrderForList>();
+                                try
+                                {
+                                    ordersForList = bl.Order.GetList();
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine(e);
+                                    break;
+                                }
+                                foreach (BO.OrderForList order in ordersForList)
+                                    Console.WriteLine(order);
+                                break;
+                            case SubMenu_Order.GetTracking:
+                                Console.WriteLine("Please enter order id");
+                                success = int.TryParse(Console.ReadLine(), out id);
+                                if (success)
+                                {
+                                    BO.OrderTracking orderTracking = new BO.OrderTracking();
+                                    try
+                                    {
+                                        orderTracking = bl.Order.GetTracking(id);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine(e);
+                                        break;
+                                    }
+                                    Console.WriteLine(orderTracking);
+                                }
+                                break;
+                            case SubMenu_Order.UpdateOrderDelivery:
+                                Console.WriteLine("Please enter order id");
+                                success = int.TryParse(Console.ReadLine(), out id);
+                                if (success)
+                                {
+                                    BO.Order order = new BO.Order();
+                                    try
+                                    {
+                                        order = bl.Order.UpdateOrderDelivery(id);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine(e);
+                                        break;
+                                    }
+                                    Console.WriteLine(order);
+                                }
+                                break;
+                            case SubMenu_Order.UpdateOrderSheep:
+                                Console.WriteLine("Please enter order id");
+                                success = int.TryParse(Console.ReadLine(), out id);
+                                if (success)
+                                {
+                                    BO.Order order = new BO.Order();
+                                    try
+                                    {
+                                        order = bl.Order.UpdateOrderSheep(id);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine(e);
+                                        break;
+                                    }
+                                    Console.WriteLine(order);
+                                }
+                                break;
+                            default:
+                                throw new InvalidInputFormatException("Invalid number:\n Please enter one of the numbers shown in the menu \n");
+                                break;
+
+                        }
+                    }
+                    else
+                        throw new InvalidInputFormatException("Please entry only a intiger Number\n");
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e);
+                }
+                Console.WriteLine("press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
             } while (!exit);
         }
     }
