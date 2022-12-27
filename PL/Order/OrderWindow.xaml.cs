f21ff05a9fa3e1dcd5c100837120878d84acba36
Avraham Meiri <ameiri181@gmail.com>
2022-12-27 10:27:00 +0200

    new windows created

---
+++ PL/Order/OrderWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.Order
+{
+    /// <summary>
+    /// Interaction logic for OrderWindow.xaml
+    /// </summary>
+    public partial class OrderWindow : Window
+    {
+        BlApi.IBl? bl = BlApi.Factory.Get();
+        public OrderWindow(int orderId, bool isEditMode)
+        {
+            InitializeComponent();
+           // OrderDetails.Text = bl?.Order.Get(orderId).ToString();
+        }
+    }
+}
