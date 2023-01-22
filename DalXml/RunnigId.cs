7ac513c6a8dd053f627656d4250b76289df58c0a
Eli Dynovisz <eli@bizmax.co.il>
2023-01-22 19:27:40 +0200

    runnig id

--- DalXml/RunnigId.cs
+++ DalXml/RunnigId.cs
@@ -3,68 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Dal
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Xml.Linq;
-
-    namespace Dal
+    internal class RunninId
     {
-        internal class RunninId
+        private int? orderId;
+        private int? orderItemId;
+
+        const string configIdPath = @"config.xml";
+        XElement ConfigRoot = null;
+
+        public int? OrderId
         {
-            private int? orderId;
-            private int? orderItemId;
-
-            const string configIdPath = @"config.xml";
-            XElement ConfigRoot = null;
-
-            public int? OrderId
+            get
             {
-                get
+                ConfigRoot = XElement.Load(configIdPath);
+                if (ConfigRoot != null)
                 {
-                    ConfigRoot = XElement.Load(configIdPath);
-                    if (ConfigRoot != null)
-                    {
-                        orderId = Convert.ToInt32(ConfigRoot.Element("orderRunningId").Value);
-                        ConfigRoot.Element("orderRunningId").SetValue(++orderId);
-                        ConfigRoot.Save(configIdPath);
-                        return orderId;
-                    }
-
-                    else
-                    {
-                        throw new ArgumentNullException("cant get an Order id");
-                    }
-
+                    orderId = Convert.ToInt32(ConfigRoot.Element("orderRunningId").Value);
+                    ConfigRoot.Element("orderRunningId").SetValue(++orderId);
+                    ConfigRoot.Save(configIdPath);
+                    return orderId;
                 }
+
+                else
+                {
+                    throw new ArgumentNullException("cant get an Order id");
+                }
+
             }
+        }
 
-            public int? OrderItemId
+        public int? OrderItemId
+        {
+            get
             {
-                get
+                ConfigRoot = XElement.Load(configIdPath);
+                if (ConfigRoot != null)
                 {
-                    ConfigRoot = XElement.Load(configIdPath);
-                    if (ConfigRoot != null)
-                    {
-                        orderItemId = Convert.ToInt32(ConfigRoot.Element("orderItemRunningId").Value);
-                        ConfigRoot.Element("orderItemRunningId").SetValue(++orderItemId);
-                        ConfigRoot.Save(configIdPath);
-                        return orderItemId;
-                    }
-
-                    else
-                    {
-                        throw new ArgumentNullException("cant get an OrderItem id");
-                    }
-
+                    orderItemId = Convert.ToInt32(ConfigRoot.Element("orderItemRunningId").Value);
+                    ConfigRoot.Element("orderItemRunningId").SetValue(++orderItemId);
+                    ConfigRoot.Save(configIdPath);
+                    return orderItemId;
                 }
+
+                else
+                {
+                    throw new ArgumentNullException("cant get an OrderItem id");
+                }
+
             }
         }
     }
-
 }
